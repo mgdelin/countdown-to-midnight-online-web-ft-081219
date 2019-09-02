b89cc5baa2c71370805c1cb93861c5ea093283ce
Mercedes Delin <mercedesgdelin@gmail.com>
2019-09-02 16:12:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
-#write your code here
-
-def countdown
+def countdown (number)
+  countdown = number
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  until countdown == 0
+    puts "HAPPY NEW YEAR!"
+  end
+countdown -= 1
+end
 end
