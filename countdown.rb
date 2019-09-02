2d78fafd357f1bc90d0f1d1a26404160f01981d7
Mercedes Delin <mercedesgdelin@gmail.com>
2019-09-02 16:40:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown (number)
+ while number > 0
+ puts "#{number} SECOND(S)!"
+ number -= 1
+ end
+ return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep (seconds)
+  number = 10
+ while number > 0
+ puts "#{number} SECOND(S)!"
+ number -= 1
+ sleep seconds
+ end
+ puts "HAPPY NEW YEAR!"
 end
