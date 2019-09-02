8b55a43c616833ffd10a13cf2b03a7f0a2f86fcb
Mercedes Delin <mercedesgdelin@gmail.com>
2019-09-02 16:28:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,16 @@ def countdown (number)
  while number > 0
  puts "#{number} SECOND(S)!"
  number -= 1
- sleep 1
  end
+ puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  number = number
+ while number > 0
+ puts "#{number} SECOND(S)!"
+ number -= 1
+ sleep 1
  end
  puts "HAPPY NEW YEAR!"
 end
